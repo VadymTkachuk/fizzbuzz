ba11edc0820cc4a8548eb0c3aba983517188528f
Vadym Tkachuk <41866278+VadymTkachuk@users.noreply.github.com>
2020-10-15 09:41:30 +0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# FizzBuzz is a simple program that prints fizz when a number
-# is divisible by three with the word "fizz"
-# and any number divisible by five with the word "buzz" 
+# FizzBuzz is a simple program that prints "fizz" when a number
+# is divisible by three and "buzz" if a number is divisible by five 
 # and "fizzbuzz" when two conditions are met.
 
 def main():
 
-    print("FizzBuzz is a simple program that prints fizz when a number\n" +
-          "is divisible by three with the word 'fizz'\n" +
-          "and any number divisible by five with the word 'buzz'\n" +
-          "and 'fizzbuzz' when two conditions are met.\n")
+    print("\nFizzBuzz is a simple program that prints \"fizz\"" + 
+          "when a number is divisible by three and \"buzz\"" + 
+          "if a number is divisible by five and \"fizzbuzz\"" +
+          "when two conditions are met.\n")
 
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
